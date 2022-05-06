3adddc84e1ed6f2ec7f5a5d4e001001741e9b816
Lyline <greg.quesne@gmail.com>
2022-05-06 09:57:33 +0200

    Configuration project Spring, jacoco,openFeign

---
+++ diabetesMainApp/src/main/java/com/medicscreen/diabetesmainapp/DiabetesMainAppApplication.java
@@ -0,0 +1,13 @@
+package com.medicscreen.diabetesmainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabetesMainAppApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DiabetesMainAppApplication.class, args);
+  }
+
+}
---
+++ diabetesMainApp/src/test/java/com/medicscreen/diabetesmainapp/DiabetesMainAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medicscreen.diabetesmainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabetesMainAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
